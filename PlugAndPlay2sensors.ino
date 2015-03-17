5c87bcf9738f8a657e163e81d5d2897e1d4b11c2
Mitch <soneen11@yahoo.com>
2015-03-17 17:35:51 -0400

    new file:   HC-SR04 single.ino.txt 	new file:   PlugAndPlay1.ino 	new file:   PlugAndPlay2sensors.ino 	new file:   libraries/Tone/Tone.cpp 	new file:   libraries/Tone/Tone.h 	new file:   libraries/Tone/changelog.txt 	new file:   libraries/Tone/examples/DTMFTest/DTMFTest.pde 	new file:   libraries/Tone/examples/RTTTL/RTTTL.pde 	new file:   libraries/Tone/examples/ToneTest/ToneTest.pde 	new file:   libraries/Tone/keywords.txt 	new file:   libraries/Ultrasonic/Ultrasonic.cpp 	new file:   libraries/Ultrasonic/Ultrasonic.h 	new file:   libraries/Ultrasonic/examples/LCD/LCD.ino 	new file:   libraries/Ultrasonic/examples/PlugAndPlay/PlugAndPlay.ino 	new file:   libraries/Ultrasonic/examples/Serial/Serial.ino 	new file:   libraries/Ultrasonic/examples/Timing/Timing.ino 	new file:   libraries/Ultrasonic/examples/TwoOrMore/TwoOrMore.ino 	new file:   libraries/Ultrasonic/keywords.txt 	new file:   libraries/readme-ultrasonic.txt 	new file:   libraries/readme.txt 	new file:   ultrasonic.ino

---
+++ PlugAndPlay2sensors.ino
@@ -0,0 +1,23 @@
+// Ultrasonic - Library for HR-SC04 Ultrasonic Ranging Module.
+// Rev.4 (06/2012)
+// J.Rodrigo ( http://www.jra.so )
+// more info at http://www.ardublog.com
+// Wiki: https://github.com/elrodri/Ultrasonic-HC-SR04/wiki/Plug-&-Play
+
+#include <Ultrasonic.h>
+
+Ultrasonic ultrasonic1(5,6); // (Trig PIN,Echo PIN)
+Ultrasonic ultrasonic2(3,4);  //(trig and echo)
+void setup() {
+  Serial.begin(9600);
+  
+}
+
+void loop()
+{
+  Serial.print(ultrasonic1.Ranging(CM)); // CM or INC
+  Serial.println(" cm1" );
+  Serial.print(ultrasonic2.Ranging(CM)); // CM or INC
+  Serial.println(" cm2" );
+  delay(1000);
+}
---
+++ ultrasonic.ino
@@ -0,0 +1,63 @@
+/*####################################################################
+ FILE: ultrasonictheramin.ino
+ VERSION: 1S0A
+ PURPOSE: The magical ultrasonic Theramin annoyingly awesome!
+ LICENSE: GPL v3 (http://www.gnu.org/licenses/gpl.html)
+
+ GET UPDATES: https://www.virtuabotix.com/
+
+ HISTORY:
+ Joseph Dattilo (Virtuabotix LLC) - Version 1S0A (03/03/13)
+ -first release.
+#######################################################################*/
+
+#include <Ultrasonic.h>
+#include <Tone.h>
+int myNote = 0;
+
+//*****************CHANGE THIS STUFF*************
+//*****************see what happens ;)***********
+int myHighestNote=NOTE_G6;
+int myLowestNote = NOTE_A4;
+
+int myGreatestDistance = 30;
+int myLowestDistance = 8;
+//***********************************************
+
+int myAudioRange = myHighestNote - myLowestNote;
+int myWorkingRange = myGreatestDistance-myLowestDistance;
+
+Tone notePlayer; //you may have to update your copy of the tone library for it to work on the new IDE
+
+
+
+Ultrasonic ultrasonic(D2, D2, BUSA);
+
+void setup()
+{
+  
+	notePlayer.begin(BUSA.P2);
+	
+}
+
+void loop()
+{
+  float inMsec;
+  long microsec = ultrasonic.timing();
+
+  inMsec = ultrasonic.convert(microsec, Ultrasonic::IN);
+  
+  if(inMsec >= myLowestDistance && inMsec <=myGreatestDistance)
+  {
+  inMsec = inMsec - 4;
+  long myNote= (myWorkingRange/inMsec) * myAudioRange + myLowestNote;
+  notePlayer.play(myNote);
+  delay(50);
+  }
+  else
+  {
+    notePlayer.stop(); //if you want it to be even more annoying comment this line to get continues tones
+  }
+    
+}
+
