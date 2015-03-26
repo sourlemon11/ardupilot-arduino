c996ebc91a5f68ecddad272665bc103d4f6f0f03
sourlemon11 <soneen11@yahoo.com>
2015-03-26 04:39:02 -0400

    modified:   arduPardu.ino 	new file:   libraries/light-sensor/Light_Sensor.cpp 	new file:   libraries/light-sensor/Light_Sensor.h

--- arduPardu.ino
+++ arduPardu.ino
@@ -4,8 +4,42 @@
 #include<ardu2ardu.h>
 
 void setup(){
-  
+ 	serial.begin(9600); 
+	const uint8_t ultrasonicPins=[];
+	const uint8_t lightPins=[];
+	const uint8_t moisturePins=[];
+	const uint8_t ardu2arduPins=[];
+	int *p;
 }
 
-void loop(){
+void loop()
+{
+	
+	/*//////////////////////////////
+	* Ultrasonic
+	*///////////////////////////////
+
+	/*//////////////////////////////
+	* Photovoltaic
+	*///////////////////////////////
+
+	for(int i=0;i<sizeof(lightPins
+
+	/*//////////////////////////////
+	* Moistue
+	*///////////////////////////////
+
+	/*//////////////////////////////
+	* I2C
+	*///////////////////////////////
+}
+
+uint16_t readAnalogPins(int pins)
+{
+	uint16_t output;
+	for(int i=0;i<sizeof(pins)/sizeof(uint8_t);i++)
+	{
+		p=pins[i];
+		readAnalog(p);
+	};
 }
