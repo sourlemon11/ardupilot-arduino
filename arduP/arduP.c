b6de0b9b6c19dcf87825593f6d252bfe2e004d76
sourlemon11 <soneen11@yahoo.com>
2015-04-22 16:39:13 -0400

    modified:   arduP 	modified:   arduP.c 	deleted:    arduP.ino 	modified:   makefile 	deleted:    test 	deleted:    test.c
     Untracked files:
    	"\001\240RJ@8\037c\006@8"
    	.arduP.c.swp
    	"\\"
    	bin/
    	test/
    	../test.c

--- arduP/arduP.c
+++ arduP/arduP.c
@@ -3,84 +3,60 @@
 //#include<moisture-sensor.h>
 //#include<ardu2ardu.h>
 #include<stdio.h>
-//#include"Arduino.h"
+#include<Arduino.h>
+#define NUM_ELEM(x) (sizeof (x) / sizeof (*(x)))
 
-struct Pins{
-    const int ultrasonicPins[4];
-    const int lightPins[4][2];
-    const int moisturePins[2];
-    const int ardu2arduPins[2];
-    int *pval;
+uint8_t ultrasonicPins[4];
+uint8_t lightPins[2][4]={1,2,3,4,5,6,7,8};
+uint8_t moisturePins[2];
+uint8_t ardu2arduPins[2];
+uint8_t *toPins;
 
-};
+// get the size of the array num elem rows * num elem columns
+uint8_t nLight = NUM_ELEM(lightPins)*sizeof(lightPins[0]) / sizeof(lightPins[0][0]); 
+uint8_t nMoisture = NUM_ELEM(lightPins); //get the size of the array
+uint8_t nUltrasonic = NUM_ELEM(lightPins); //get the size of the array
 
-struct Values{
-    float lightPinVals[4][2];
-    float moisturePinVals[2];
-    float ultrasonicPinVals[4];
-};
+float lightPinVals[2][4];
+float avgPinVal[4];
+float moisturePinVals[2];
+float ultrasonicPinVals[4];
 
-//return pointer to array
-float * readAnalogPins(const int *pins,int n)
+// read each Pin val w/pointers.
+void readAnalogPins(uint8_t *toPins,float *toPinVals,int nElem)
 {
-    
-    for(int i=0;i<n;i++)
+    int i;
+    for(i=0; i<nElem; i++)
     {
-        printf("pins: %d\n",pins[i]);
-        pval=valueOfPins;
+        *(toPinVals+i) = analogRead(*(toPins+i));
     };
 };
 
-//int getNumberofElements(int array[])
-//{
-//    int n=0; //indices
-//    int i=0; //counter
-//    while(n!=NULL)
-//    {   
-//        n=array[i]
-//        i++;
-//    };
-//    return n;
-//};
-//
-int main(int argc, char *argv[])
+// take the average between two pin values 
+float * avgTwoPinVal(float pinVals[][m],int n, int m)
 {
-   // Serial.println(SIZEOFPINS);
-	//Serial.println("AHHHH");
-	/*
-	* Ultrasonic
-	*/
-
-	/*
-	* Photovoltaic
-	*/
-    int n = sizeof(lightPins)/sizeof(lightPins[0]); //get the size of the array
-    printf("size: %d\n",n); //check
-
-    readAnalogPins(lightPins,n,lightPinVals);// read the all the analog pins inside the array
-
-
-
-	/*
-	* Moisture
-	*/
-
-	/*
-	* I2C
-	*/
-    return 0;
-}
+    int j;
+    float avgPinVal[m];
+    
+    for(j=0; j < m ; i++)
+    {
+        avgPinVal[j] = 0.5 * ( pinVals[j][0]+pinVals[j][1]);
+    };
+};
 
 void setup(){
-// 	Serial.begin(9600); 
-        // loop through all the arrays, and assign pins
-        //for(int i=0;
-        
+    uint8_t i=1;
+    //set all pins with pointers
+    for(i=0; i < nLight; i++)
+    {
+        pinMode(*(*lightPins+i),INPUT);
+    }; 
 };
 
 void loop()
 {
-   // Serial.println(SIZEOFPINS);
+    // 	Serial.begin(9600); 
+    // Serial.println(SIZEOFPINS);
 	//Serial.println("AHHHH");
 	/*
 	* Ultrasonic
@@ -89,6 +65,10 @@ void loop()
 	/*
 	* Photovoltaic
 	*/
+    //toPins=&lightPins[0][0];
+    //float *toLightPinVals=&lightPinVals[0][0];
+    printf("size: %d\n",nLight); //check
+    readAnalogPins(&lightPins[0][0],&lightPinVals[0][0],nLight);
 
 	/*
 	* Moisture
@@ -99,3 +79,12 @@ void loop()
 	*/
 };
 
+int main()
+{
+    int i;
+    for( i=0; i < nLight; i++)
+    {
+        printf("LIGHTPIN:%d \n",*lightPins[i]);
+    }; 
+    return 0;
+};
